f01ff0b75b79fd35ed506ee229de8128d91611ac
yusefzaman <yusef96@gmail.com>
2024-05-17 19:02:18 +0300

    update of game funcitons to display box value

--- Script.js
+++ Script.js
@@ -1,68 +1,142 @@
 // GLobal variables
-const gameBoard = document.getElementById('wholeBoard').innerText
+// const gameBoard = document.getElementById('wholeBoard').innerText
+console.log(document.getElementById('wholeBoard'))
 const eachBox = document.getElementsByClassName('innerBoard')
-// eachBox will be used for event listener and then to check if it is a match - when display of textInside is set to none, innerText of eachBox is empty.
-
 const textInside = document.getElementsByClassName('innertext')
-// textInside will be used for hide or show text by setting display to none
 
-// textInside[1].style.display = 'flex'
-// the above will set the innerText to null
+let cardOne = null
+let cardTwo = null
+// Declare and initialize cardOne and cardTwo
+
+document.querySelectorAll('.innerBoard').forEach((parentElement) => {
+  parentElement.addEventListener('click', (event) => {
+    // console.log('You clicked me')
+    const childElement = event.target.firstChild
+    console.log(childElement)
+    const value = childElement.innerText
+    console.log(value)
+    // Display the childElement
+    childElement.style.display = 'flex'
+    if ((cardOne = null)) {
+      const cardOne = value
+    } else if (card) {
+    }
+    // const cardOne = value
+    // If this is the second card that was flipped over check if it is a match
+
+    // If they match then run match logic / function
+    // If they don't match hide both cards
+  })
+})
+
+console.log(cardOne)
+// Create a function that check if two cards are a match
+
+// Create a function that handles a match
+// Inside this function make the two cards that match unclickable
+// Also inside this function check if the game is over
+
+// // GLobal variables
+// const gameBoard = document.getElementById('wholeBoard').innerText
+// const eachBox = document.getElementsByClassName('innerBoard')
+// // eachBox will be used for event listener and then to check if it is a match - when display of textInside is set to none, innerText of eachBox is empty.
+
+// const textInside = document.getElementsByClassName('innertext')
+// // textInside will be used for hide or show text by setting display to none
+
+// // textInside[1].style.display = 'flex'
+// // the above will set the innerText to null
 
 // console.log(eachBox[1].innerText)
-// the above displays will log the innterText of index 1 cell. when display: none, value will be empty
+// // the above displays will log the innterText of index 1 cell. when display: none, value will be empty
 
-// const gettingValue = eachBox[1].innerText
+// // const gettingValue = eachBox[1].innerText
 
-// create an event listenser that when the eachBox is clicked, it will toggle display to flex, if not
+// // create an event listenser that when the eachBox is clicked, it will toggle display to flex, if not
+// // const displayedBoxes = []
 
-function clickToDisplay() {
-  for (let i = 0; i > eachBox.length; i++) {
-    textInside[i].style.display = 'flex'
-  }
-}
+// // console.log(textInside[0].innerText)
 
-for (let i = 0; i > eachBox.length; i++) {
-  eachBox[i].addEventListener('click', () => {
-    clickToDisplay()
-  })
-}
-
-// function checkMatch() {
-//   if (eachBox[0].innerText === eachBox[6].innerText) {
-//     // console.log(document.getElementById('wholeBoard').innerText)
-//     return true
-//   } else {
-//     return false
-//   }
-// }
-// const winCaseOne = if (eachBox[0].innerText === eachBox[6].innerText) {
-//   return true
-// } else {
-//   return false
-// }
-// const winCaseTwo = if (eachBox[0].innerText === eachBox[6].innerText) {
-//   return true
+// function clickToDisplay1() {
+//   textInside[0].style.display = 'flex'
+//   // displayedBoxes.splice(0, 0, 1),
+//   console.log(textInside[0].innerText)
 // }
 
-// ...
-
-// Funcitons for the game
-
-// // add click event listeners
-// function clickFlipper() {
-//   for (let i = 0; i > eachBox.length; i++) {
-//     eachBox[0].addEventListeners('click', () => {
-//       // document.getElementById('one').style.color = 'black'
-//     })
-//   }
+// function clickToDisplay1() {
+//   textInside[1].style.display = 'flex'
+//   // displayedBoxes.splice(0, 0, 1),
+//   console.log(textInside[1].innerText)
 // }
 
-// function checkMatch() {}
+// // eachBox[0].addEventListener('click', () => {
+// //   clickToDisplay1()
+// // })
 
-// start fucntion
-// const startGame = () => {
-//   addEventListeners()
-// }
+// // function clickToDisplay2() {
+// //   textInside[1].style.display = 'flex',
+// //   displayedBoxes.splice(0, 0, 1)
+// // }
+// // eachBox[1].addEventListener('click', () => {
+// //   clickToDisplay2()
+// // })
 
-// startGame()
+// // console.log(displayedBoxes)
+// // console.log(if(eachBox[3].innerText === eachBox[2].innerText){return true})
+
+// // function clickToDisplay() {
+// //   for (let i = 0; i > eachBox.length; i++) {
+// //     eachBox[i].addEventListener('click', () => {
+// //       textInside[i].style.display = 'flex'
+// //       console.log(clickToDisplay())
+// //     })
+// //   }
+// // }
+
+// // function clickToDisplay() {
+// //   textInside[6].style.display = 'flex'
+// // }
+// // console.log(clickToDisplay())
+
+// // eachBox[6].addEventListener('click', () => {
+// //   clickToDisplay()
+// //   console.log(clickToDisplay())
+// // })
+// // function checkMatch() {
+// //   if (eachBox[0].innerText === eachBox[6].innerText) {
+// //     // console.log(document.getElementById('wholeBoard').innerText)
+// //     return true
+// //   } else {
+// //     return false
+// //   }
+// // }
+// // const winCaseOne = if (eachBox[0].innerText === eachBox[6].innerText) {
+// //   return true
+// // } else {
+// //   return false
+// // }
+// // const winCaseTwo = if (eachBox[0].innerText === eachBox[6].innerText) {
+// //   return true
+// // }
+
+// // ...
+
+// // Funcitons for the game
+
+// // // add click event listeners
+// // function clickFlipper() {
+// //   for (let i = 0; i > eachBox.length; i++) {
+// //     eachBox[0].addEventListeners('click', () => {
+// //       // document.getElementById('one').style.color = 'black'
+// //     })
+// //   }
+// // }
+
+// // function checkMatch() {}
+
+// // start fucntion
+// // const startGame = () => {
+// //   addEventListeners()
+// // }
+
+// // startGame()
